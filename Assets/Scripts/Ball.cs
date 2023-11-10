6fb6132c88220b5d29016455011564ee438bf5e7
HTL2910 <hathanhlong2910@gmail.com>
2023-11-10 17:24:04 +0700

    add object

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private bool smash;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            smash = true;
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            smash=false;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            smash = true;
+            rb.velocity = new Vector3(0f,100 * Time.deltaTime * 7, 0f);
+        }
+        if(rb.velocity.y>5f)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 5f, rb.velocity.z);
+        }    
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!smash)
+        {
+            rb.velocity=new Vector3(0f,50* Time.deltaTime*5, 0f);
+        }
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!smash || collision.gameObject.CompareTag("Finish"))
+        {
+
+            rb.velocity = new Vector3(0f, 50 * Time.deltaTime * 5, 0f);
+        }
+    }
+}
